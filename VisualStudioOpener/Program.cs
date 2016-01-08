abd7cfdb46f30fa6365b900f6b3d2c344a08e1b9
Håvard Rustad Olsen <havard.olsen@knowit.no>
2016-01-08 09:48:42 +0100

    init

---
+++ VisualStudioOpener/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioOpener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = @"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe";
+            info.UseShellExecute = true;
+            info.Verb = "runas"; // Provides Run as Administrator
+            Process.Start(info);
+
+        }
+    }
+}
